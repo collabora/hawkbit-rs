e8fff9f2c3357607b10d33576de50af1b47bf2f7
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-12-15 12:18:09 +0100

    doc: include all public API in doc

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@
 // SPDX-License-Identifier: MIT
 
 pub mod direct_device_integration;
-pub use direct_device_integration::DirectDeviceIntegration;
-
-mod config_data;
-pub use config_data::Mode;
 
 mod common;
-pub use common::{Execution, Finished};
+mod config_data;
 mod deployment_base;
 mod feedback;
 mod poll;
+
+pub use common::{Execution, Finished};
+pub use config_data::{Mode, Request};
+pub use deployment_base::{
+    Artifact, Chunk, DownloadedArtifact, MaintenanceWindow, Type, Update, UpdatePreFetch,
+};
+pub use direct_device_integration::DirectDeviceIntegration;
+pub use poll::Reply;
